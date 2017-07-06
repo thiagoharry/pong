c15f17acf8f97737f98e28b308ee50df94bd4cb6
Thiago "Harry" Leucz Astrizi <harry@bitbitbit.com.br>
2017-07-06 10:46:24 -0300

    Corrigido o licenciamento.

--- src/copyleft_image.c
+++ src/copyleft_image.c
@@ -1,3 +1,22 @@
+/*
+Copyright (c) Thiago Leucz Astrizi, 2017
+
+This file is part of pong.
+
+pong is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pong is distributed in the hope that it will be useful,
+but WITHOUT ANY  WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS  FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more  details.
+
+You should have received a copy of the GNU General Public License
+along with pong. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* GIMP RGBA C-Source image dump (copyleft.c) */
 
 // 256x64, 4 bytes/pixel
